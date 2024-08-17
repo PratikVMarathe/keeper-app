8f6219ecdbead094f5d75d9334bd24e3c952527e
PratikVMarathe <86453383+PratikVMarathe@users.noreply.github.com>
2024-08-17 11:24:07 +0530

    Keeper-app-part3 added new components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+// import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (newNote) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  };
+
+  const deleteNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
     <div>
       <Header />
-      {notes.map(noteItem => <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} />)}
-      
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+
+      {/* use map() for iterating items of notes.jsx, adding them in Note.jsx component and showing them in UI */}
+      {/* {notes.map(noteItem => <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} />)} */}
+
       <Footer />
     </div>
   );
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function CreateArea() {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setNote((prevNote) => {
+      return { ...prevNote, [name]: value };
+    });
+  };
+
+  const submitNote = (event) =>{
+    props.onAdd(note);
+    setNote({
+        title: "",
+        content: "",
+    });
+    event.preventDefault();
+  }
+  return (
+    <div>   
+      <form>
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
